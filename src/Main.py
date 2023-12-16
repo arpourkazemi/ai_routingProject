9efd8dd86b4105be535ef04d5357dbfc08e2a2ce
Amirreza <arpourkazemi@gmail.com>
2023-12-16 17:45:52 +0330

    delete unwanted items

--- src/Main.py
+++ src/Main.py
@@ -8,6 +8,7 @@ from IDS import IDS
 from UCS import UCS
 from BestFS import BestFS
 from Astar import Astar
+
 file_path = './test/1.txt'
 
 matrix = Input.read_file(file_path)
@@ -16,86 +17,10 @@ board = Board(matrix)
 
 initial_state = State(board)
 
-print(board.is_equal(initial_state.board))
-
-for state in initial_state.successor():
-    print(board.is_equal(state.board))
-
-
-print("---------------------")
-#b= BFS(initial_state)
-#d= DFS(initial_state)
-#i= IDS(initial_state)
-#u= UCS(initial_state)
-#best = BestFS(initial_state)
+# print("---------------------")
+# b = BFS(initial_state)
+# d = DFS(initial_state)
+# i = IDS(initial_state)
+# u = UCS(initial_state)
+# best = BestFS(initial_state)
 A = Astar(initial_state)
-"""
-['1R', '1', '1', '5', '5', '4', '2C', '1', '15', '1B']
-['1', '1', '5', '3', '5', '5', '4', '5', 'X', 'X']
-['5', '1I', '1', '6', '2', '2', '2', '1', '1', '1T']
-['X', 'X', '1', '6', '5', '5', '2', '1', '1', 'X']
-['X', 'X', '1', 'X', 'X', '50', '2', '1C', '1', 'X']
-['1', '1', '1', '2', '2', '2T', '2', '1', '1', '1']
-
-
-['1R', '1', '1', '5', '5', '4', '2C', '1', '15', '1B']
-['1', '1', '5', '3', '5', '5', '4', '5', 'X', 'X']
-['5', '1I', '1', '6', '2', '2', '2', '1', '1', '1T']
-['X', 'X', '1', '6', '5', '5', '2', '1', '1', 'X']
-['X', 'X', '1', 'X', 'X', '50', '2', '1C', '1', 'X']
-['1', '1', '1', '2', '2', '2T', '2', '1', '1', '1']
-"""
-
-#d= DFS1(board)
-#d.dfs(initial_state,0)
-#d.plot_dfs_tree()
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.down)
-
-# initial_state.print()
-
-# initial_state.move(moves.down)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.right)
-
-# initial_state.print()
-
-# initial_state.move(moves.left)
-
-# initial_state.print()
